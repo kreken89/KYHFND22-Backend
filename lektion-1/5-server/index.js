74f83d88dbb241c039e5143fb5d39c232e64a224
joakim w <joakim.wahlstrom@epndata.com>
2023-02-28 14:45:06 +0100

    lektion 1

--- lektion-1/5-server/index.js
+++ lektion-1/5-server/index.js
@@ -1,10 +1,40 @@
 const http = require('http');
+const fs = require('fs')
+const path = require('path')
+
 
 const server = http.createServer((req, res) => {
-  // console.log('testing')
-  // console.log(req.url)
-  // res.write('<h1>HELLO</h1>')
-  res.end('<h1>HELLO</h1>')
+
+  if(req.url === '/favicon.ico') return
+
+  let fileName;
+
+  switch(req.url) {
+    case '/':
+      fileName = 'index.html'
+      res.statusCode = 200
+      res.setHeader('Content-type', 'text/html')
+      break;
+    case '/about':
+      fileName = 'about.html'
+      break;
+    default:
+      fileName = 'notfound.html'
+      res.statusCode = 404
+      break;
+  }
+  
+  const filePath = path.join(__dirname, 'src', fileName)
+
+  fs.readFile(filePath, (err, data) => {
+    if(err){
+      console.log(err)
+      return
+    }
+
+    res.end(data)
+  })
+
 })
 
 server.listen(9999, () => console.log('server runnig on http://localhost:9999'))
