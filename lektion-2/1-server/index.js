c26c1f08056c5cdefc35ab5452ebbe5bc8ba6467
joakim w <joakim.wahlstrom@epndata.com>
2023-03-02 09:16:53 +0100

    lektion 2

--- lektion-2/1-server/index.js
+++ lektion-2/1-server/index.js
@@ -2,7 +2,21 @@ const http = require('http');
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  const readFileAndRespond = (path) => {
+  
+  // console.log(req.url) // talar om för oss vad användaren försöker hämta hem,
+
+  switch(req.url) {
+    case '/':
+      readFileAndRespond('./views/index.html')
+      break;
+    case '/about':
+      readFileAndRespond('./views/about.html')
+      break;
+    default:
+      readFileAndRespond('./views/404.html')
+  }
+  function readFileAndRespond(path) {
+    console.log(path)
     fs.readFile(path, (err, data) => {
       if(err) {
         console.log(err)
@@ -12,18 +26,6 @@ const server = http.createServer((req, res) => {
       res.end(data)
     })
   }
-  // console.log(req.url) // talar om för oss vad användaren försöker hämta hem,
-
-  switch(req.url) {
-    case '/':
-      readFileAndRespond('./views/index.html')
-    case '/about':
-      readFileAndRespond('./views/about.html')
-    default:
-      readFileAndRespond('./views/404.html')
-  }
-
-
   
 })
 
