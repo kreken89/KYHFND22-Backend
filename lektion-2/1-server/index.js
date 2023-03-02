7059dac7501d0ca11def7b47d543bba4fd46b1e9
joakim w <joakim.wahlstrom@epndata.com>
2023-03-02 10:01:26 +0100

    lektion 2

--- lektion-2/1-server/index.js
+++ lektion-2/1-server/index.js
@@ -8,18 +8,29 @@ const server = http.createServer((req, res) => {
   switch(req.url) {
     case '/':
       readFileAndRespond('./views/index.html')
+      res.statusCode = 200
       break;
     case '/about':
       readFileAndRespond('./views/about.html')
+      res.statusCode = 200
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about')
+      res.end()
       break;
     default:
       readFileAndRespond('./views/404.html')
+      res.statusCode = 404
   }
+
+
   function readFileAndRespond(path) {
     console.log(path)
     fs.readFile(path, (err, data) => {
       if(err) {
         console.log(err)
+        res.statusCode = 500
         return res.end('<h1>errrror</h1>')
       }
 
