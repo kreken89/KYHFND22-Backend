9d62a4d35087d780f8dd1a1461ce5a0d0f72b314
joakim w <joakim.wahlstrom@epndata.com>
2023-03-30 12:06:07 +0200

    lektion 10

--- lektion-9/1-blog/models/postModel.js
+++ lektion-9/1-blog/models/postModel.js
@@ -2,13 +2,18 @@ const Post = require('../schemas/postSchema');
 
 exports.createNewPost = async (req, res) => {
 
-  const { title, body, imgURL, author, tags } = req.body;
+  // const { title, body, imgURL, author, tags } = req.body;
+  const { title, body, imgURL, tags } = req.body;
 
-  if(!title || !body || !imgURL || !author) {
+  // if(!title || !body || !imgURL || !author) {
+  //   return res.status(400).json({ message: 'You need to enter all the fields' })
+  // }
+  if(!title || !body || !imgURL) {
     return res.status(400).json({ message: 'You need to enter all the fields' })
   }
 
-  const post = await Post.create({ title, body, imgURL, author, tags })
+  // const post = await Post.create({ title, body, imgURL, author, tags })
+  const post = await Post.create({ title, body, imgURL, author: req.userId, tags })
 
   if(!post) {
     return res.status(500).json({ message: 'Something went wrong when creating the post' })
