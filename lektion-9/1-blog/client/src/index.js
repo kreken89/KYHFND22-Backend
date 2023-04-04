b69102e34700dac641f9304f4ebe2aedbcf6938c
joakim w <joakim.wahlstrom@epndata.com>
2023-04-04 13:55:26 +0200

    lektion 11

--- lektion-9/1-blog/client/src/index.js
+++ lektion-9/1-blog/client/src/index.js
@@ -22,5 +22,45 @@ const verifyToken = () => {
     location.replace('login.html')
   }
 }
-
 verifyToken()
+
+const getPosts = async () => {
+  const res = await fetch('http://localhost:7778/api/blog')
+  const posts = await res.json()
+
+
+  posts.forEach(post => {
+
+    document.querySelector('#output').insertAdjacentHTML('beforeend', `
+    <div class="post">
+        <div class="img-container">
+          <img src="${post.imgURL}" alt="${post.title}">
+        </div>
+        <div class="right">
+          <div class="top">
+            <div>
+              <h3>${post.title}</h3>
+              <small>${post.author.firstName} ${post.author.lastName}</small>
+            </div>
+            <ul id="tags${post._id}" class="tags">
+            </ul>
+          </div>
+          <div class="body">
+            <p class="snippet">${post.body.slice(0, 50)}...</p>
+            <p class="likes">likes: <span>${post.likes}</span></p>
+          </div>
+        </div> 
+      </div>
+    `)
+    const ul = document.querySelector('#tags' + post._id)
+    post.tags.forEach(tag => {
+      const li = document.createElement('li')
+      li.className = 'tag'
+      li.innerText = tag
+      ul.append(li)
+    })
+
+  })
+
+}
+getPosts()
